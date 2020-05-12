9d2fce6753fea0ae72cc89345dd00d9b674f3d57
casikami <fede@barriola.com.uy>
2020-05-12 17:22:41 +0200

    long lat

--- client/src/components/Maps/Places.js
+++ client/src/components/Maps/Places.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import {geocodeByAddress, getLatLng} from 'react-google-places-autocomplete';
 // If you want to use the provided css
 import 'react-google-places-autocomplete/dist/index.min.css';
  
 class Component extends React.Component {
 
     onInputSelect = spotLocation => {
+        geocodeByAddress(spotLocation.description)
+        .then(results => getLatLng(results[0]))
+        .then(({ lat, lng }) =>
+          spotLocation.coordinates = { lat, lng }
+        )
         this.props.getLocation(spotLocation)
     }
+
+   
 
   render(){
       return(
