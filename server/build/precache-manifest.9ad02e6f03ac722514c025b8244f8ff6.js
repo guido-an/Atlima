68c483f792f67c22f062bb9e61f2b5491007e546
Guido <carucciguido@gmail.com>
2020-06-07 20:57:29 +0200

    deployment

---
+++ server/build/precache-manifest.9ad02e6f03ac722514c025b8244f8ff6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a3c86dfdb4ec20b0b7a17220829e32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddc4f969cc8f70dbdef6",
+    "url": "/static/css/2.ad28d71b.chunk.css"
+  },
+  {
+    "revision": "0547587c5bb9eb5594f8",
+    "url": "/static/css/main.c7a11ee7.chunk.css"
+  },
+  {
+    "revision": "ddc4f969cc8f70dbdef6",
+    "url": "/static/js/2.adab6956.chunk.js"
+  },
+  {
+    "revision": "0547587c5bb9eb5594f8",
+    "url": "/static/js/main.17d3435d.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "246bfd3e214b39a304aca84fcb1d290f",
+    "url": "/static/media/Sport Image-1.246bfd3e.png"
+  },
+  {
+    "revision": "096115c4d20862b2981d17a70baed622",
+    "url": "/static/media/Sport Image-2.096115c4.png"
+  },
+  {
+    "revision": "7e3cdc0e00017865c325d435d94a52cb",
+    "url": "/static/media/Sport Image-3.7e3cdc0e.png"
+  },
+  {
+    "revision": "839919af2c203ca03bf4c871faf004d0",
+    "url": "/static/media/Sport Image-4.839919af.png"
+  },
+  {
+    "revision": "60f8314547d47aa9a47e98ba519cf81f",
+    "url": "/static/media/Sport Image-5.60f83145.png"
+  },
+  {
+    "revision": "dd83215ac93dabdc53463f43637c194f",
+    "url": "/static/media/Sport Image.dd83215a.png"
+  }
+]);
