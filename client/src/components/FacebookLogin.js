10fcc6263a6bde56529fd7118c01bd7daf2e1a7a
Guido <carucciguido@gmail.com>
2020-06-07 21:22:29 +0200

    test deploy

--- client/src/components/FacebookLogin.js
+++ client/src/components/FacebookLogin.js
@@ -5,7 +5,7 @@ class FacebookLogin extends React.Component {
   render () {
     return (
       <div>
-        <a href='http://localhost:5000/auth/facebook' className='facebook-btn'><i className="fab fa-facebook"></i>Login with Facebook</a>
+        <a href='http://altima-app.herokuapp.com/auth/facebook' className='facebook-btn'><i className='fab fa-facebook' />Login with Facebook</a>
       </div>
     )
   }
