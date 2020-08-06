e97b58397b56025c3274ddfbe5ca5dcf809acc45
Guido <carucciguido@gmail.com>
2020-08-06 11:55:32 +0200

    production

---
+++ server/build/precache-manifest.e526a2746d8d7c7c18953530bc38c3ee.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1deed7adeefb72a0a9074c21ff95db26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d146890e4727d03dce77",
+    "url": "/static/css/2.c3f2a436.chunk.css"
+  },
+  {
+    "revision": "8fcf96d03c0ae401b999",
+    "url": "/static/css/main.d0e19079.chunk.css"
+  },
+  {
+    "revision": "d7e5533b1df837431c40",
+    "url": "/static/js/10.dfa40ccb.chunk.js"
+  },
+  {
+    "revision": "414efd8c44aeb00a7265",
+    "url": "/static/js/11.402373a3.chunk.js"
+  },
+  {
+    "revision": "1183de442d191e7cd9be",
+    "url": "/static/js/12.44db94a5.chunk.js"
+  },
+  {
+    "revision": "9f53c7f75dbd3450c85a",
+    "url": "/static/js/13.c617013f.chunk.js"
+  },
+  {
+    "revision": "4838c5fd486862976749",
+    "url": "/static/js/14.1d4046a6.chunk.js"
+  },
+  {
+    "revision": "d146890e4727d03dce77",
+    "url": "/static/js/2.4d76ab55.chunk.js"
+  },
+  {
+    "revision": "657d14c77b6152f107e4",
+    "url": "/static/js/3.aa855cf9.chunk.js"
+  },
+  {
+    "revision": "60cc4ecd44353c5d4eb4",
+    "url": "/static/js/4.021b2e39.chunk.js"
+  },
+  {
+    "revision": "dcdcd867ca6fc0f79da2",
+    "url": "/static/js/5.3eb7268f.chunk.js"
+  },
+  {
+    "revision": "7b841dfaaf86d193c739",
+    "url": "/static/js/6.662d0acf.chunk.js"
+  },
+  {
+    "revision": "6cc1a47b1395ba42a181",
+    "url": "/static/js/7.34ab1daf.chunk.js"
+  },
+  {
+    "revision": "59b079905254c57fc665",
+    "url": "/static/js/8.e06b3460.chunk.js"
+  },
+  {
+    "revision": "655daa8d316571182fa8",
+    "url": "/static/js/9.1b9a7ad1.chunk.js"
+  },
+  {
+    "revision": "8fcf96d03c0ae401b999",
+    "url": "/static/js/main.1dc50fd7.chunk.js"
+  },
+  {
+    "revision": "a2d65ee6d37d1a8e53bc",
+    "url": "/static/js/runtime-main.b5e63b91.js"
+  },
+  {
+    "revision": "246bfd3e214b39a304aca84fcb1d290f",
+    "url": "/static/media/Sport Image-1.246bfd3e.png"
+  },
+  {
+    "revision": "096115c4d20862b2981d17a70baed622",
+    "url": "/static/media/Sport Image-2.096115c4.png"
+  },
+  {
+    "revision": "7e3cdc0e00017865c325d435d94a52cb",
+    "url": "/static/media/Sport Image-3.7e3cdc0e.png"
+  },
+  {
+    "revision": "839919af2c203ca03bf4c871faf004d0",
+    "url": "/static/media/Sport Image-4.839919af.png"
+  },
+  {
+    "revision": "60f8314547d47aa9a47e98ba519cf81f",
+    "url": "/static/media/Sport Image-5.60f83145.png"
+  },
+  {
+    "revision": "dd83215ac93dabdc53463f43637c194f",
+    "url": "/static/media/Sport Image.dd83215a.png"
+  },
+  {
+    "revision": "258b029df6c0755eaca722f5464fbffe",
+    "url": "/static/media/boxing.258b029d.jpg"
+  },
+  {
+    "revision": "b04b8c504474cb3333947ca6c945407f",
+    "url": "/static/media/cycling.b04b8c50.jpg"
+  },
+  {
+    "revision": "4e2535872ee0d0ebce2ab4cd63c50c8f",
+    "url": "/static/media/discobolo.4e253587.jpg"
+  },
+  {
+    "revision": "fd3671b10d418dffb15560c02542b2fe",
+    "url": "/static/media/fitness.fd3671b1.jpg"
+  },
+  {
+    "revision": "c00576165862a2b19f33d81ef37e8b20",
+    "url": "/static/media/landing.c0057616.jpeg"
+  },
+  {
+    "revision": "18ff49adb3c8dfe21de2ee45f67bd33c",
+    "url": "/static/media/landing2.18ff49ad.gif"
+  },
+  {
+    "revision": "e19eac8e1b4976d5f79b2629d5cda110",
+    "url": "/static/media/landing3.e19eac8e.jpeg"
+  },
+  {
+    "revision": "e5cc4b0d87d8bd1c3b0137551849ee31",
+    "url": "/static/media/landing4.e5cc4b0d.jpeg"
+  },
+  {
+    "revision": "1dfe2cc378ebcb9f17c452ad3a733475",
+    "url": "/static/media/rollerskating.1dfe2cc3.jpeg"
+  },
+  {
+    "revision": "b284688b11b993082150d9337a526a9c",
+    "url": "/static/media/running.b284688b.jpg"
+  },
+  {
+    "revision": "2366214c3a62f92f86f6a8644851be4d",
+    "url": "/static/media/surfing.2366214c.jpg"
+  }
+]);
