416a714e63af0c30164e3e89ad92e4315c403479
Guido <carucciguido@gmail.com>
2020-08-07 17:35:15 +0200

    fixes

--- server/build/service-worker.js
+++ server/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3090cc8386287fc45c66950c5172920.js"
+  "/precache-manifest.db739c3339c60ff60eea3ab7aba70c7f.js"
 );
 
 self.addEventListener('message', (event) => {
